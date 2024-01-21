b4fea6ff7e882dfd5fb63b309918222485517616
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-01-21 14:30:32 +0800

    r

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Authenticatable
     */
     public function getFullNameAttribute(): string
     {
-        return ucwords($this->firstname) . ' ' . ucwords($this->lastname);
+        return $this->firstname . ' ' . $this->lastname;
     }
 
     public function getAgeAttribute()
